77e22516d9056ae94f8eace80bae6d784f9658e6
Vesta-nna <anna.dominguez.anna@gmail.com>
2025-02-06 19:03:28 +0100

    card done

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,25 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 export default {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
-  },
-  plugins: [],
+	content: [
+		'./src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/components/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/app/**/*.{js,ts,jsx,tsx,mdx}',
+	],
+	theme: {
+		extend: {
+			colors: {
+				background: '#F2EAE2',
+				metal: '#6C7289',
+				gunMetal: '#1C232B',
+				aquamarine: '#3D8168',
+				aquamarineHover: '#1A4032',
+			},
+			fontFamily: {
+				sans: ['Montserrat', 'sans-serif'],
+				mono: ['Fraunces', 'serif'],
+			},
+		},
+	},
+	plugins: [],
 } satisfies Config;
